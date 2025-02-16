c15d00ef3534f144f7dbc804a3fcf75cc08675e2
Hridoy <154232641+1dev-hridoy@users.noreply.github.com>
2025-02-16 19:18:07 +0600

    added admin files

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php include 'includes/sidebar.php'; ?>
+<?php include 'includes/navbar.php';?>
+<main class="h-full pb-16 overflow-y-auto">
+          <!-- Remove everything INSIDE this div to a really blank page -->
+          <div class="container px-6 mx-auto grid">
+            <h2
+              class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+            >
+              Blank
+            </h2>
+          </div>
+        </main>
+
+<?php include 'includes/footer.php'; ?>
